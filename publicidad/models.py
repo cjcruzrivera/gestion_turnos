1245ae0d6fecf25b7b80aeabe623cafd88a9e672
melissamillares <diana.millares@correounivalle.edu.co>
2019-03-20 10:52:37 -0500

    creacion app publicidad

--- publicidad/models.py
+++ publicidad/models.py
@@ -4,7 +4,7 @@ class Publicidad(models.Model):
     codigo = models.CharField(max_length = 15, primary_key = True)
     nombre = models.CharField(max_length = 100)
     descripcion = models.CharField(max_length = 300)
-    imagen = models.ImageField(upload_to = '/gestion_turnos/images')
+    imagen = models.ImageField(upload_to = 'gestion_turnos/images')
 
 def __str__(self):
     return self.nombre
