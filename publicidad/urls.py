8a623ddad0c5bce6238ba869151aaea054128829
melissamillares <diana.millares@correounivalle.edu.co>
2019-03-20 14:54:08 -0500

    cambios en templates

--- publicidad/urls.py
+++ publicidad/urls.py
@@ -7,6 +7,6 @@ from .views import PublicidadDeleteView
 urlpatterns = [
     path('', PublicidadListView.as_view(), name='list_publicidad'),
     path('create/', PublicidadCreateView.as_view(), name='create_publicidad'), 
-    path('update/', PublicidadUpdateView.as_view(), name='update_publicidad'),   
+    path('update/<int:pk>', PublicidadUpdateView.as_view(), name='update_publicidad'),   
     path('delete/<int:pk>', PublicidadDeleteView.as_view(), name='delete_publicidad'),
 ]
