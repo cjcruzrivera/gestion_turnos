4e2083fe07c83c648ba0cf6f761e2ce9c78f8af6
Camilo Cruz <cruz.camilo@correounivalle.edu.co>
2019-03-21 15:22:07 -0500

    mover templates a carpeta independiente

--- gestion_turnos/settings.py
+++ gestion_turnos/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'gestion_turnos.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR,'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -130,13 +130,8 @@ STATIC_ROOT = os.path.join(os.path.dirname(
     os.path.dirname(__file__)), 'static', 'static-only')
 SITE_ROOT = os.path.dirname(os.path.realpath(__file__))
 
-STATICFILES_DIRS = (
-    os.path.join(SITE_ROOT, 'static/'),
-    os.path.join(os.path.dirname(
-        os.path.dirname(__file__)), 'static', 'static'),
-)
 
-TEMPLATES_DIRS = (
-    os.path.join(os.path.dirname(os.path.dirname(__file__)),
-                 'static', 'templates')
-)
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+    '/var/www/static/',
+]
