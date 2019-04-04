30c7d1d1d804f19cf07aa425700f31c9c259d013
melissamillares <diana.millares@correounivalle.edu.co>
2019-04-04 15:08:03 -0500

    cambios publicidad

--- publicidad/models.py
+++ publicidad/models.py
@@ -4,7 +4,15 @@ class Publicidad(models.Model):
     codigo = models.CharField(max_length = 15, primary_key = True)
     nombre = models.CharField(max_length = 100)
     descripcion = models.CharField(max_length = 300)
-    imagen = models.ImageField(upload_to = 'publicidad')
+    imagen = models.ImageField(upload_to = 'publicidad/')
+
+    @staticmethod
+    def get_publicidad():
+        try:
+            publicidad = Publicidad.objects.all()
+            return publicidad
+        except Publicidad.DoesNotExist:
+            return None
 
 def __str__(self):
     return self.nombre
