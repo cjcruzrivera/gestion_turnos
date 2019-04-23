038bb48eeaebdc461928977fd99ff30a37d023c9
melissamillares <diana.millares@correounivalle.edu.co>
2019-04-22 19:53:34 -0500

    owl-carousel para mostrar la publicidad

--- publicidad/admin.py
+++ publicidad/admin.py
@@ -1,10 +1,4 @@
 from django.contrib import admin
 from .models import Publicidad
 
-class PublicidadAdmin(admin.ModelAdmin): 
-    list_display = ["nombre"]
-
-    class Meta:
-        model = Publicidad
-
 admin.site.register(Publicidad)
